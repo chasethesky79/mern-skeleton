a45297b58adcd5ba72c5cb83d9d89c154ffb474b
Bharath Seshadri <Bharath.Seshadri@charter.com>
2021-09-21 13:02:51 +0300

    Initial changes

--- Chapter03 and 04/mern-skeleton/client/core/Home.js
+++ Chapter03 and 04/mern-skeleton/client/core/Home.js
@@ -5,6 +5,7 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import unicornbikeImg from './../assets/images/unicornbike.jpg'
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -32,20 +33,21 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export default function Home(){
-  const classes = useStyles()
-    return (
-        <Card className={classes.card}>
-          <Typography variant="h6" className={classes.title}>
-            Home Page
+  const classes = useStyles();
+  return (
+      <Card className={classes.card}>
+        <Typography variant="h6" className={classes.title}>
+          Home Page
+        </Typography>
+        <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle"/>
+        <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
+        <CardContent>
+          <Typography variant="body1" component="p">
+            Welcome to the MERN Skeleton home page.
           </Typography>
-          <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle"/>
-          <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
-          <CardContent>
-            <Typography variant="body1" component="p">
-              Welcome to the MERN Skeleton home page.
-            </Typography>
-          </CardContent>
-        </Card>
-    )
+        </CardContent>
+        <Link to='/users'>Users</Link>
+      </Card>
+  )
 }
 
--- Chapter03 and 04/mern-skeleton/client/user/Signup.js
+++ Chapter03 and 04/mern-skeleton/client/user/Signup.js
@@ -1,109 +1,91 @@
-import React, {useState} from 'react'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import Icon from '@material-ui/core/Icon'
-import { makeStyles } from '@material-ui/core/styles'
-import {create} from './api-user.js'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogTitle from '@material-ui/core/DialogTitle'
+import React from 'react'
+import { Typography, Card, CardContent, CardActions, Button, Dialog, DialogActions, DialogContent, DialogTitle, DialogContentText, TextField } from '@material-ui/core';
+import { useState } from 'react';
+import { create } from './api-user';
+import { makeStyles } from '@material-ui/core/styles';
 import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 600,
-    margin: 'auto',
-    textAlign: 'center',
-    marginTop: theme.spacing(5),
-    paddingBottom: theme.spacing(2)
-  },
-  error: {
-    verticalAlign: 'middle'
-  },
-  title: {
-    marginTop: theme.spacing(2),
-    color: theme.palette.openTitle
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 300
-  },
-  submit: {
-    margin: 'auto',
-    marginBottom: theme.spacing(2)
-  }
-}))
-
-export default function Signup() {
-  const classes = useStyles()
-  const [values, setValues] = useState({
-    name: '',
-    password: '',
-    email: '',
-    open: false,
-    error: ''
-  })
-
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value })
-  }
-
-  const clickSubmit = () => {
-    const user = {
-      name: values.name || undefined,
-      email: values.email || undefined,
-      password: values.password || undefined
+    card: {
+      maxWidth: 600,
+      margin: 'auto',
+      textAlign: 'center',
+      marginTop: theme.spacing(5),
+      paddingBottom: theme.spacing(2)
+    },
+    error: {
+      verticalAlign: 'middle'  
+    },
+    title: {
+       marginTop: theme.spacing(2),
+       color: theme.palette.openTitle 
+    },
+    textField: {
+       marginLeft: theme.spacing(1),
+       marginRight: theme.spacing(1),
+       width: 300
+    },
+    submit: {
+        margin: 'auto',
+        marginBottom: theme.spacing(2)
     }
-    create(user).then((data) => {
-      if (data.error) {
-        setValues({ ...values, error: data.error})
-      } else {
-        setValues({ ...values, error: '', open: true})
-      }
-    })
-  }
+  }))
 
-    return (<div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Sign Up
-          </Typography>
-          <TextField id="name" label="Name" className={classes.textField} value={values.name} onChange={handleChange('name')} margin="normal"/><br/>
-          <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal"/><br/>
-          <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
-          <br/> {
-            values.error && (<Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>error</Icon>
-              {values.error}</Typography>)
-          }
-        </CardContent>
-        <CardActions>
-          <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
-        </CardActions>
-      </Card>
-      <Dialog open={values.open} disableBackdropClick={true}>
-        <DialogTitle>New Account</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            New account successfully created.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Link to="/signin">
-            <Button color="primary" autoFocus="autoFocus" variant="contained">
-              Sign In
-            </Button>
-          </Link>
-        </DialogActions>
-      </Dialog>
-    </div>
+export default function SignUp() {
+    const classes = useStyles();
+    const [values, setValues] = useState({
+      name: '',
+      email: '',
+      password: '',
+      open: false,
+      error: ''
+    })
+
+    const handleChange = name => event => {
+        const { target: { value }} = event;
+        setValues({...values, [name]: value });
+    }
+
+    const handleSubmit = async () => {
+        const { name, email, password } = values;
+        const user = {
+            name,
+            email,
+            password
+        };
+        const { error } = await create(user);
+        setValues(error ? { ...values, error} : {...values, open: true });
+    };
+
+    return (
+        <div>
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography variant="h6" className={classes.title}>
+                        Sign Up
+                    </Typography>
+                    <TextField id='name' label='Name' className={classes.textField} value={values.name} onChange={handleChange('name')} margin='normal'/><br/>
+                    <TextField id='email' type='email' label='Email' className={classes.textField} value={values.email} onChange={handleChange('email')} margin='normal'/><br/>
+                    <TextField id='password' type='password' label='Password' className={classes.textField} value={values.password} onChange={handleChange('password')} margin='normal'/><br/>
+                    {values.error && <Typography component='p' color='error'><Icon color='error' className={classes.error}>error</Icon>{values.error}</Typography>}
+                </CardContent>
+                <CardActions>
+                    <Button color='primary' variant='contained' onClick={handleSubmit} className={classes.submit}>Submit</Button>
+                </CardActions>
+            </Card>
+            <Dialog open={values.open} disableBackdropClick={true}>
+                <DialogTitle>New Account</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>New account successfully created</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Link to='/signin'>
+                        <Button color='primary' autoFocus='autoFocus' variant='contained'>
+                            Sign In
+                        </Button>
+                    </Link>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
